7a59cf3c3c3d369ff1063f42000c419ba26bbe8e
Raman <RamanMadan@gmail.com>
2022-02-03 21:51:41 +0000

    testing flask

--- hello2.py
+++ hello2.py
@@ -1,3 +1,9 @@
-msg2 = "hello 2"
-print(msg2)
+from flask import Flask
+app=Flask(__name__)
 
+@app.route("/")
+def hello():
+    return "why Hello world again"
+
+if __name__ == "__main__":
+    app.run()
