2bcfbbb9f2fe902b070372dfd7cd138fe824f815
Raman <RamanMadan@gmail.com>
2022-02-03 22:02:46 +0000

    new routes

--- hello2.py
+++ hello2.py
@@ -2,8 +2,17 @@ from flask import Flask
 app=Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
     return "why Hello world again"
+
+@app.route("/hello")
+def hello():
+    return "HELLO there"
+    
+@app.route("/members")
+def members():
+    return "HELLO there Members"
+
 
 if __name__ == "__main__":
     app.run()
